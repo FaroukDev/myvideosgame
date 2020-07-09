d9314aaf097117db537700ef66780b65dfe803d3
FaroukDev <fa.elayache1@gmail.com>
2020-07-09 12:23:04 +0200

    filter games by category in Gametable

--- src/components/GameTable.js
+++ src/components/GameTable.js
@@ -12,17 +12,22 @@ class GameTable extends Component {
     render() {
         const filterText = this.props.filterText;
         const inStockOnly = this.props.inStockOnly;
+        let lastCat;
 
         const rows = [];
         this.props.jeux.forEach(jeu => {
-            console.log("jeu", jeu.name);
+            //console.log("jeu", jeu.name);
             if(jeu.name.indexOf(filterText) === -1 ) {
                 return;
             }
             if(inStockOnly && !jeu.stocked) {
                 return;
             }
+            if(jeu.category !== lastCat) {
+                rows.push(<GameCategory category={jeu.category}/>)
+            }
             rows.push(<GameRow key={jeu.name} jeu={jeu}/>)
+            lastCat = jeu.category;
             
         });
         //rows.push(<GameRow key="1"/>);
